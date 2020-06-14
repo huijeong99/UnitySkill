721f7ce74d044876171df96d8dfc061008a8f2f9
hwayuya <hwayuya@gmail.com>
2020-06-14 23:25:17 +0900

    유니티 스킬 10. 유도미사일

---
+++ UnitySkill/Assets/Scripts/MissileLauncher/Missile.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    Rigidbody m_rigid = null;
+    Transform m_tfTarget = null;
+
+    [SerializeField] float m_speed = 0f;
+    float m_currentSpeed = 0f;
+    [SerializeField] LayerMask m_layerMask = 0;
+    [SerializeField] ParticleSystem m_psEffect = null;
+
+    void SearchEnemy()
+    {
+        Collider[] t_cols = Physics.OverlapSphere(transform.position, 100f, m_layerMask);
+
+        if (t_cols.Length > 0)
+        {
+            m_tfTarget = t_cols[Random.Range(0, t_cols.Length)].transform;
+        }
+    }
+
+    IEnumerator LaunchDelay()
+    {
+        yield return new WaitUntil(() => m_rigid.velocity.y < 0f);
+        yield return new WaitForSeconds(0.1f);
+
+        SearchEnemy();
+        m_psEffect.Play();
+
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigid = GetComponent<Rigidbody>();
+        StartCoroutine(LaunchDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_tfTarget != null)
+        {
+            if (m_currentSpeed <= m_speed)
+                m_currentSpeed += m_speed * Time.deltaTime;
+
+            transform.position += transform.up * m_currentSpeed * Time.deltaTime;
+
+            Vector3 t_dit = (m_tfTarget.position - transform.position).normalized;//거리가 의미없기때문에 방향만 가져옴
+            transform.up = Vector3.Lerp(transform.up, t_dit, 0.25f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("EnemyCube"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
