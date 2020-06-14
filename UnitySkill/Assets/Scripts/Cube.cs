7e1f1fcde67c37a55a3e2920683f817e8700189d
hwayuya <hwayuya@gmail.com>
2020-06-14 17:14:56 +0900

    유니티 스킬 1. 오브젝트 풀링 기법

---
+++ UnitySkill/Assets/Scripts/Cube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    Rigidbody m_myRigid = null;
+
+    private void OnEnable()
+    {
+        if (m_myRigid == null)
+        {
+            m_myRigid = GetComponent<Rigidbody>();
+        }
+
+        m_myRigid.velocity = Vector3.zero;
+        m_myRigid.AddExplosionForce(1000, transform.position, 1f);
+
+        StartCoroutine(DestroyCube());
+    }
+
+    IEnumerator DestroyCube()
+    {
+        yield return new WaitForSeconds(1f);
+        ObjectPoolingManager.instance.InsertQueue(gameObject);
+    }
+}
